635a7fb218378474741b8701c3c2d5434b1a7e0d
Kristine Fernandez <kfernandez@mightycitizen.com>
2021-03-10 10:49:04 -0600

    add json with gunfighter data; add gatsby plugin to read that data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
+        `gatsby-transformer-json`,
+        {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+            path: `./src/data/`,
+          },
+        },
 
     ]
 }
