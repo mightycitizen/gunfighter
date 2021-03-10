0db7aabbba45f4d7626eabdd6a17026be96908f3
Kristine Fernandez <kfernandez@mightycitizen.com>
2021-03-10 11:10:45 -0600

    create graphql query, output array of fighters

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,48 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby";
 
 // markup
 const IndexPage = () => {
+  const fightersQuery = graphql`{
+    allGunfightersJson {
+      edges {
+        node {
+          id
+          ambidextrous
+          bac
+          braveryacc
+          braveryspd
+          bsp
+          exp
+          firstname
+          gac
+          lastname
+          loss
+          nickname
+          profession
+          rank
+          seed
+          spd
+          st
+          win
+          winvshigher
+          winvstop10
+          winvstop5
+          wpn
+        }
+      }
+    }
+  }`;
+  const response = useStaticQuery(fightersQuery);
+  const fighters = response.allGunfightersJson.edges;
   return (
     <main>
-
+      <ul>
+      { fighters.map((fighter, i) =>  {
+        const data = fighter.node;
+        return <li key={ i }>{ data.firstname } { data.lastname }</li>
+      })}
+      </ul>
     </main>
   )
 }
